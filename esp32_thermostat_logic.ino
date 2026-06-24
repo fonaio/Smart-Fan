e3ca0b9e13cdd1f6ae78922cebb09b957f100c40
Fiona Lin <flin3004825@gmail.com>
2026-06-24 14:40:08 -0500

    Added Loop code for physical remote
    Currently thinking of alternative architecture to prevent irregular speeds when sleeping as well- maybe create a system where I can override the max speed, but the LED cannot??

--- esp32_thermostat_logic.ino
+++ esp32_thermostat_logic.ino
@@ -1,55 +1,57 @@
-#include <Arduino.h>
+//rework logic to see if the temperature still has not reached the desired amt we increase to max speed??
+//i can override max_speed with the remote (ex go higher), but the LED cannot
+//think of alternative architecture to go for
+#include <Arduino.h> 
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 #include <IRrecv.h>
 #include <IRutils.h>
 #include <DHT.h>
 
-#define IR_LED_PIN      1
+#define IR_LED_PIN 1
 #define IR_RECIEVE_PIN 5
-#define DHT_PIN         4
+#define DHT_PIN 4
 
 const int CHECK_TEMP_INTERVAL = 30000;
-const int TARGET_SPEED = 4;
 const float TEMP_LOWER_BOUND = 20.0;
 const float TEMP_UPPER_BOUND = 22.0;
 const int CARRIER_FREQUENCY = 38;
+const int MAX_SPEED = 6; 
 
 bool fanOn = false;
 int currentSpeed = 1;
 
-
 const uint16_t POWER_RAW[] = {
   1250, 450,
- 1250, 400, 400, 1250, 1250, 450, 1200, 450,
- 400, 1200, 450, 1250, 450, 1200, 450, 1200,
- 450, 1200, 450, 1200, 1300, 7400, 1250, 450,
- 1200, 450, 400, 1250, 1250, 400, 1250, 450,
- 350, 1250, 450, 1200, 450, 1250, 450, 1200,
- 450, 1200, 450, 1200, 1250, 7450, 1250, 400,
- 1250, 450, 400, 1200, 1250, 450, 1250, 400,
- 400, 1250, 400, 1250, 450, 1200, 450, 1200,
- 500, 1150, 500, 1200, 1250, 7400, 1250, 450,
- 1250, 400, 400, 1250, 1250, 450, 1200, 450,
- 400, 1200, 450, 1250, 450, 1200, 450, 1200,
- 450, 1200, 450, 1200, 1250, 7450, 1250, 450,
- 1200, 450, 400, 1250, 1250, 400, 1250, 400,
- 400, 1250, 450, 1200, 450, 1200, 500, 1200,
- 450, 1200, 450, 1200, 1250
+  1250, 400, 400, 1250, 1250, 450, 1200, 450,
+  400, 1200, 450, 1250, 450, 1200, 450, 1200,
+  450, 1200, 450, 1200, 1300, 7400, 1250, 450,
+  1200, 450, 400, 1250, 1250, 400, 1250, 450,
+  350, 1250, 450, 1200, 450, 1250, 450, 1200,
+  450, 1200, 450, 1200, 1250, 7450, 1250, 400,
+  1250, 450, 400, 1200, 1250, 450, 1250, 400,
+  400, 1250, 400, 1250, 450, 1200, 450, 1200,
+  500, 1150, 500, 1200, 1250, 7400, 1250, 450,
+  1250, 400, 400, 1250, 1250, 450, 1200, 450,
+  400, 1200, 450, 1250, 450, 1200, 450, 1200,
+  450, 1200, 450, 1200, 1250, 7450, 1250, 450,
+  1200, 450, 400, 1250, 1250, 400, 1250, 400,
+  400, 1250, 450, 1200, 450, 1200, 500, 1200,
+  450, 1200, 450, 1200, 1250
 };
 
 const uint16_t SPEED_UP_RAW[] = {
   1250, 450,
- 1200, 450, 400, 1200, 1300, 400, 1250, 400,
- 400, 1250, 450, 1200, 450, 1200, 500, 1200,
- 1250, 400, 400, 1250, 450
+  1200, 450, 400, 1200, 1300, 400, 1250, 400,
+  400, 1250, 450, 1200, 450, 1200, 500, 1200,
+  1250, 400, 400, 1250, 450
 };
 
 const uint16_t SPEED_DOWN_RAW[] = {
-1250, 400,
-1250, 450, 400, 1200, 1250, 450, 1250, 400,
-400, 1250, 400, 1250, 450, 1200, 450, 1200,
-450, 1250, 1250, 400, 400
+  1250, 400,
+  1250, 450, 400, 1200, 1250, 450, 1250, 400,
+  400, 1250, 400, 1250, 450, 1200, 450, 1200,
+  450, 1250, 1250, 400, 400
 };
 
 const uint16_t POWER_RAW_LEN = sizeof(POWER_RAW) / sizeof(POWER_RAW[0]);
@@ -59,20 +61,20 @@ const uint16_t SPEED_DOWN_RAW_LEN = sizeof(SPEED_DOWN_RAW) / sizeof(SPEED_DOWN_R
 IRsend irsend(IR_LED_PIN);
 IRrecv irrecv(IR_RECIEVE_PIN);
 decode_results results;
-DHT    dht(DHT_PIN, DHT11);
+DHT dht(DHT_PIN, DHT11);
 
 void sendPower() {
-  //sends singal to change the power state
+  //sends signal to change the power state
   Serial.println("IR is sending POWER");
   irsend.sendRaw(POWER_RAW, POWER_RAW_LEN, CARRIER_FREQUENCY);
   delay(200);
 }
 
 void sendSpeedUp(){
-  //Sends signal to increase the speed of the fan. Speed increases by 1 only if the current speed is less than 4.
+  //Sends signal to increase the speed of the fan. Speed increases by 1 only if the current speed is less than TARGET_SPEED.
   Serial.println("IR is sending SPEED UP");
   irsend.sendRaw(SPEED_UP_RAW, SPEED_UP_RAW_LEN, 38);
-  if (currentSpeed < 4){
+  if (currentSpeed < MAX_SPEED){
     currentSpeed ++;
   }
   delay(200);
@@ -86,27 +88,12 @@ void sendSpeedDown(){
     currentSpeed --;
   }
   delay(200);
-  
-}
-
-void setFanSpeed(){
-  //sets the speed of the fan to TARGET_SPEED
-  Serial.println("Setting the fan speed to " + String(TARGET_SPEED));
-  while (currentSpeed < TARGET_SPEED){
-    sendSpeedUp();
-    delay(500);
-  }
-  while (currentSpeed > TARGET_SPEED){
-    sendSpeedDown();
-    delay(500);
-  }
 }
 
 void turnfanOn(){
   Serial.println("Turning the fan ON");
   sendPower();
   delay(1500);
-  setFanSpeed();
   fanOn = true;
 }
 
@@ -116,11 +103,26 @@ void turnfanOff(){
   delay(1500);
   fanOn = false;
 }
-void setup() {
-
+ 
+void setup(){
+  Serial.begin(115200);
+  irsend.begin();
+  irrecv.enableIRIn();
+  dht.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  //if we recieve an IR signal from the physical remote
+  if (irrev.decode(&results)) {
+    //Speed up signal recieved from physical remote
+    if (resultToSourceCode(&results).indexOf("SPEED_UP_RAW") != 1) {
+      if (currentSpeed == 9){
+        currentSpeed = 1;
+        Serial.println("Physical remote detected. Speed is now: " + currentSpeed);
+      } 
+      else {
+        currentSpeed ++;
+        Serial.println("Physical remote detected. Speed is now: " + currentSpeed);
+      }
+  }
 }
